9de180d63587d0bd2bcd7042acb619becc91f1ac
Jean-Paul <94898715+Lollynixt@users.noreply.github.com>
2025-07-11 14:03:07 +0100

    Update SSC.py

--- SSC.py
+++ SSC.py
@@ -6,4 +6,12 @@ dic = pd.read_csv("C:\\Users\\jeanp\\Documents\\GitHub\\SSC\\Structure Calc - Di
 
 dic = dic.dropna()
 
+dic = pd.DataFrame(dic)
+
 print(dic.to_string())
+
+dic_group = dic['Megastructure']
+
+dic_group = dic['Megastructure'].drop_duplicates()
+
+print(dic_group)
